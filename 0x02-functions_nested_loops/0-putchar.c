dc90511de269b959c67ee19cdd9f965a02d85c8c
kadelite <mechatronics.samson@gmail.com>
2022-09-13 05:57:10 -0700

    task0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description - A program that print putchar
+ *
+ * Return: Always return (0)
+ */
+
+int main(void)
+{
+	putchar ("_putchar");
+
+	return (0);
+}

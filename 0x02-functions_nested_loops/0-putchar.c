8eb71acf5486517c931d56958d32c81e4ad063a3
kadelite <mechatronics.samson@gmail.com>
2022-09-13 07:20:01 -0700

    tsk0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Prints _putchar
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	_putchar('_putchar');
+	_putchar('\n');
 
+	return (0);
+}

db9e0cff78dbbea83a4ea1a0a391635354a6e10d
kadelite <mechatronics.samson@gmail.com>
2022-09-13 13:02:48 -0700

    task0

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,5 @@
-#ifndef _ALX_
-#define _ALX_
+#ifndef _main_h_
+#define _main_h_
 
 int _putchar(char c);
 void print_alphabet(void);
@@ -13,5 +13,6 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
+void print_times_tables(int n);
 
-#endif /* _ALX */
+#endif

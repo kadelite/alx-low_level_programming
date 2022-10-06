3c36d3008298a2b61f98b32404aad89d7795d416
kadelite <mechatronics.samson@gmail.com>
2022-10-06 14:12:30 -0700

    task 101

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,10 +1,6 @@
 #ifndef _main_h_
 #define _main_h_
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <limits.h>
-
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);

0dfedb02e9fe7b11392c604a8950a9617414995a
kadelite <mechatronics.samson@gmail.com>
2022-10-06 14:06:42 -0700

    task 101

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,14 +1,15 @@
 #ifndef _main_h_
 #define _main_h_
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-int _checknum(char *argv[], int n);
-char *_initialize_array(char *ar, int lar);
-void _is_zero(char *argv[]);
 
 #endif
